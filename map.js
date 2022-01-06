ee71b579cd879f9cd3a39a7a46bd1229e2266652
pipjivko <pipjivko@gmail.com>
2022-01-06 10:31:27 +0200

    added marker

--- map.js
+++ map.js
@@ -1,8 +1,32 @@
-function initMap(){
-   var map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 42.6333, lng: 24.822},
-  zoom: 16
-  })
-  return map
-}
+function initMap() {
+
+    const myLocation = {lat: 42.63376, lng: 24.816163};
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: myLocation,
+        zoom: 18
+    });
+    
+    
+
+   const marker =  new google.maps.Marker({
+    position: myLocation,
+    map: map,
+    title:"Hello Petko",
+    visible:true , 
+    
+    });
+    
+  var infoWindow = new google.maps.InfoWindow(
+          {
+              content:" my habitate",
   
+    } )
+    marker.addListener(
+            'click',()=>{
+    infoWindow.open(map,marker);   
+            }
+            )
+    
+}
+
