55a23c1810f80ac076f097b6de21d7b58a7bf4af
pipjivko <pipjivko@gmail.com>
2022-03-22 13:58:38 +0200

    simple map

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        
+        <link rel="stylesheet" href="map2.css"/>
+    </head>
+    <body>
+        <div id='map'></div>
+        <script src="map2.js"></script>
+         <script  async defer
+     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDcmzbIGPZnyIpC4YioPEmzMvk11CdCv38&callback=initMap"
+     
+    ></script>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ map2.js
@@ -0,0 +1,40 @@
+var coor = {}
+function initMap() {
+    const myLatlng = {lat: 42.635, lng: 24.82};
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: myLatlng
+    });
+    // Create the initial InfoWindow.
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click the map to get Lat/Lng!",
+        position: myLatlng,
+
+    });
+
+    infoWindow.open(map);
+    //Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+
+        infoWindow.close();
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        coor.lat = JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2);
+
+
+        var marker = new google.maps.Marker({
+            position: mapsMouseEvent.latLng,
+            map: map,
+            title: "very good",
+            content: "Pecata"
+        })
+        marker.addListener('click', () => {
+            infoWindow.setContent("Karlovo")
+            infoWindow.open(map)
+        })
+
+    });
+
+
+}
